62636de06f60c379d5bf631a42cff63ea0bbf54f
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-07-02 11:08:05 +0000

    Add function to get human choice

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ function getComputerChoice() {
     return options[Math.floor(Math.random() * 3)];
 }
 
+function getHumanChoice() {
+    let validatedInput = false;
+
+    while (validatedInput === false) {
+        const choice = prompt("Rock, paper or scissors", options[Math.floor(Math.random() * 3)]);
+
+        if (choice == null) {
+            continue;
+        }
+
+        const validatedChoice = choice.trim().toLowerCase();
+        if (options.includes(validatedChoice)) {
+            validatedInput = true;
+            return validatedChoice;
+        }
+    }
+}
